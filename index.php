274dcebb693d5e1bfc40c5b3cc87d83b9f1dfd6a
Nguyen Huu Tuan Minh <45720038+tuanminh318@users.noreply.github.com>
2019-09-02 22:02:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>\
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <link rel="stylesheet" type="text/css" href="style.css">
 <!------ Include the above in your HEAD tag ---------->
  <title>Login</title>
