30ac061107b0ed1ab26cbbf242ffbbea87692e26
Nguyen Huu Tuan Minh <45720038+tuanminh318@users.noreply.github.com>
2019-09-02 21:55:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/style.css">
-    <title>Login</title>
-</head>
-
-<body>
-    <div>
-        <h1>Login</h1>
-        <form action="managing.php" method="POST">
-            <input type="text" name="aduser" placeholder="Username"> <br>
-            <input type="password" name="adpass" placeholder="Password"> <br>
-            <input type="submit" value="Login">
-        </form>
-</body>
-
-</html>
+ 
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>\
+<link rel="stylesheet" type="text/css" href="style.css">
+<!------ Include the above in your HEAD tag ---------->
+ <title>LOGIN</title>
+<section class="login-block">
+    <div class="container">
+    <div class="row">
+        <div class="col-md-4 login-sec">
+            <h2 class="text-center">Login Now</h2>
+            <form class="login-form">
+  <div class="form-group">
+    <label for="exampleInputEmail1" class="text-uppercase">Username</label>
+    <input type="text" class="form-control" placeholder="">
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1" class="text-uppercase">Password</label>
+    <input type="password" class="form-control" placeholder="">
+  </div>
+  
+  
+    <div class="form-check">
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input">
+      <small>Remember Me</small>
+    </label>
+    <button type="submit" class="btn btn-login float-right">Submit</button>
+  </div>
+  
+</form>
+<div class="copy-text"><i class="fa fa-heart"></i></div>
+        </div>
+        <div class="col-md-8 banner-sec">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                 <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+            <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>TOY STORE</h2>
+            <p></p>
+        </div>
+        </div>
+    </div>
+</div>
+</section>
